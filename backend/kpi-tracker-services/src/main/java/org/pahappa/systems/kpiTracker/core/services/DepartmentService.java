4bbe8cfad23ad1e087559b2c5abaf56067ec28de
ssenyonjoalvin <alvinssenyonjo9@gmail.com>
2025-08-12 10:12:43 +0300

    Added the department class for adding new department Mgt

---
+++ backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/services/DepartmentService.java
@@ -0,0 +1,6 @@
+package org.pahappa.systems.kpiTracker.core.services;
+
+import org.pahappa.systems.kpiTracker.models.department.Department;
+
+public interface DepartmentService extends GenericService<Department> {
+}
