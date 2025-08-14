d9dd8566fb55eccc48770193c1a56c8013270c94
helviz <twomoelvisrodney@gmail.com>
2025-08-14 08:33:46 +0300

    feat: add DAO interfaces and their implementations
    - Created AssignedUserDao, DepartmentDao, TeamDao interfaces definitions for data access operations
    - Implemented corresponding DAO classes with CRUD and query methods
    - Ensured consistency with existing service layer structure

---
+++ backend/kpi-tracker-services/src/main/java/org/pahappa/systems/kpiTracker/core/dao/DepartmentDao.java
@@ -0,0 +1,6 @@
+package org.pahappa.systems.kpiTracker.core.dao;
+
+import org.pahappa.systems.kpiTracker.models.department.Department;
+
+public interface DepartmentDao extends BaseDao<Department> {
+}
