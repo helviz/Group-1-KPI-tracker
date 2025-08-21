ad0f7e6994cfe461f42a05195b20181f50e6db87
helviz <twomoelvisrodney@gmail.com>
2025-08-21 15:25:05 +0300

    ch: changed the fetch type

--- backend/kpi-tracker-models/src/main/java/org/pahappa/systems/kpiTracker/models/organisationFit/SurveyQuestion.java
+++ backend/kpi-tracker-models/src/main/java/org/pahappa/systems/kpiTracker/models/organisationFit/SurveyQuestion.java
@@ -3,16 +3,15 @@ package org.pahappa.systems.kpiTracker.models.organisationFit;
 import org.sers.webutils.model.BaseEntity;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
-@Table(
-        name="survey_question"
-)
+@Table(name = "survey_question")
 public class SurveyQuestion extends BaseEntity {
     private String question;
     private SurveyCategory category;
 
-//    getters and setters
+    // Getters and Setters
     @Column(nullable = false, length = 1000)
     public String getQuestion() {
         return question;
@@ -21,7 +20,8 @@ public class SurveyQuestion extends BaseEntity {
     public void setQuestion(String question) {
         this.question = question;
     }
-    @ManyToOne(fetch = FetchType.LAZY)
+
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "category_id", nullable = false)
     public SurveyCategory getCategory() {
         return category;
@@ -31,4 +31,29 @@ public class SurveyQuestion extends BaseEntity {
         this.category = category;
     }
 
+    // equals and hashCode
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SurveyQuestion)) return false;
+        SurveyQuestion that = (SurveyQuestion) o;
+        return Objects.equals(getId(), that.getId()) ||
+                (Objects.equals(question, that.question) &&
+                        Objects.equals(category, that.category));
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), question, category);
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return "SurveyQuestion{" +
+                "id=" + getId() +
+                ", question='" + question + '\'' +
+                ", category=" + (category != null ? category.getName() : "null") +
+                '}';
+    }
 }
